085080f74de3aa8aad40716ee095851be0a185ae
Manoj Gulati <er.manojgulati@gmail.com>
2015-08-27 15:17:09 +0530

    added new scripts

--- Analyse_stats_features.m
+++ Analyse_stats_features.m
@@ -4,14 +4,16 @@ clc;
 clear all;
 
 file_names = {'BGN_LC1','BGN_LC2','BGN_LC3','BGN_LC4','BGN_LC5','LC1','LC2','LC3','LC4','LC5','LCD1','LCD2','LCD3','LCD4','LCD5','CFL1','CFL2','CFL3','CFL4','CFL5','CPU1','CPU2','CPU3','CPU4','CPU5','PRT1','PRT2','PRT3','PRT4','PRT5'};
-Path1 = '/Users/manojgulati/Documents/Algo_Testing_Data/30_March_2015/TD16384_Features_SET1/';
-Path2 = 'FEAT50/'
-
+Path1 = '/Users/manojgulati/Documents/Algo_Testing_Data/30_March_2015/TD16384_Features_SET1/compressed_features_Test/';
+Path2 = 'FEAT150/';
 for i=1:30
-    
-    load(strcat(Path1,char(file_names(i)),'_TD_stat','.mat'));
-    eval(sprintf('X%d = feature_vector;',i));
-%     clear feature_vector;
+Temp = [];
+    for dim = 1:10
+        
+        load(strcat(Path1,Path2,char(file_names(i)),'_TD_stat_',int2str(dim),'.mat'));
+        Temp = [Temp feature(:,1)];
+    end
+    eval(sprintf('X%d = Temp;',i));
 end
 
 
--- Stats_EMI_ver0_2.m
+++ Stats_EMI_ver0_2.m
@@ -11,7 +11,7 @@ Path1 = '/Users/manojgulati/Documents/Algo_Testing_Data/30_March_2015/';
 Path2 = 'TD16384_Features_SET1/';
 Path3 = 'PRT';
 Path5 = '_TD_stat.mat';
-Path4 = 'compressed_features_Test/FEAT150/';
+Path4 = 'compressed_features_Test/FEAT300/';
 File_Path = strcat(Path1,Path2,Path3);
 temp = []; % Temp variable to store recovered desc stats features
 feature_vector = []; % Features recovered from custom descriptive statistics function
@@ -25,13 +25,13 @@ while(index<No_of_instances+1)
 
 load(strcat(Path1,Path2,Path3,int2str(index),Path5));
 
-for d_index = 1:10
+for d_index = 1:5
     for f_index = 1:15
-        temp = feature_vector(f_index,1+offset:150+offset);
+        temp = feature_vector(f_index,1+offset:300+offset);
         feature(f_index,:) = Desc_stats_ver0_2(temp);
         save(strcat(Path1,Path2,Path4,Path3,int2str(index),'_TD_stat','_',int2str(d_index),'.mat'),'feature');
     end
-offset = offset+150;    
+offset = offset+300;    
 end
 offset = 0;
 display(int2str(index));
