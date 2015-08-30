1532d909bc29688b712331c450e96da56d3d77c0
Manoj Gulati <er.manojgulati@gmail.com>
2015-08-30 16:38:00 +0530

    modified scripts

--- Analyse_stats_features_ver0_2.m
+++ Analyse_stats_features_ver0_2.m
@@ -4,13 +4,15 @@ clc;
 clear all;
 
 file_names = {'BGN_LC1','BGN_LC2','BGN_LC3','BGN_LC4','BGN_LC5','LC1','LC2','LC3','LC4','LC5','LCD1','LCD2','LCD3','LCD4','LCD5','CFL1','CFL2','CFL3','CFL4','CFL5','CPU1','CPU2','CPU3','CPU4','CPU5','PRT1','PRT2','PRT3','PRT4','PRT5'};
-Path1 = '/Users/manojgulati/Documents/Algo_Testing_Data/30_March_2015/TD16384_Features_SET3/';
+Path1 = '/Users/manojgulati/Documents/Algo_Testing_Data/30_March_2015/TD16384_Features_SET3/compressed_features_Test/';
 Path2 = 'FEAT150/';
 for i=1:30
 Temp = [];
+    for dim = 1:10
         
-    load(strcat(Path1,char(file_names(i)),'_TD_stat','.mat'));
-    Temp = [Temp feature_vector(:,:)];
+        load(strcat(Path1,Path2,char(file_names(i)),'_TD_stat_',int2str(dim),'.mat'));
+        Temp = [Temp feature(:,1)];
+    end
     eval(sprintf('X%d = Temp;',i));
 end
 
@@ -18,11 +20,11 @@ end
 %%
 temp=[];
 for j=1:30
-    eval(sprintf('temp = [temp X%d(16,:)''];',j));
-    set(gca,'FontSize', 18);
-    boxplot(temp);
-    grid on;
-    ylim([-0.0005 .1]);
+    eval(sprintf('temp = [temp; X%d(:,:)''];',j));
+%     set(gca,'FontSize', 18);
+%     boxplot(temp);
+%     grid on;
+% %     ylim([-0.0005 30]);
 %     ylabel('Amplitude (in Volts)')
     
 end
--- Stats_EMI_ver0_1.m
+++ Stats_EMI_ver0_1.m
@@ -6,13 +6,14 @@
 clear all;
 close all;
 clc;
-f_index=1;
+f_index=3;
 file_names = {'LC/BGN_LC1','LC/BGN_LC2','LC/BGN_LC3','LC/BGN_LC4','LC/BGN_LC5','LC/LC1','LC/LC2','LC/LC3','LC/LC4','LC/LC5','LCD/LCD1','LCD/LCD2','LCD/LCD3','LCD/LCD4','LCD/LCD5','CFL/CFL1','CFL/CFL2','CFL/CFL3','CFL/CFL4','CFL/CFL5','CPU/CPU1','CPU/CPU2','CPU/CPU3','CPU/CPU4','CPU/CPU5','PRT/PRT1','PRT/PRT2','PRT/PRT3','PRT/PRT4','PRT/PRT5'};
+file_names1 = {'BGN_LC1','BGN_LC2','BGN_LC3','BGN_LC4','BGN_LC5','LC1','LC2','LC3','LC4','LC5','LCD1','LCD2','LCD3','LCD4','LCD5','CFL1','CFL2','CFL3','CFL4','CFL5','CPU1','CPU2','CPU3','CPU4','CPU5','PRT1','PRT2','PRT3','PRT4','PRT5'};
 Path1 = '/Users/manojgulati/Documents/Algo_Testing_Data/30_March_2015/';
 % Path2 = '/';
 Path3 = '_';
 Path4 = 'TD16384_Features_SET3/';
-while(f_index<2)
+while(f_index<31)
     
 File_Path = strcat(Path1,char(file_names(f_index)),Path3);
 
@@ -55,7 +56,7 @@ display(int2str(index));
 index=index+1;
 end
 
-save(strcat(Path1,Path4,Path3,'TD_stat','.mat'),'feature_vector');
+save(strcat(Path1,Path4,char(file_names1(f_index)),Path3,'TD_stat','.mat'),'feature_vector');
 
 f_index=f_index+1;
 
